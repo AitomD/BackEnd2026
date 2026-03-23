850373035b5e8fb978458c48b574a1bf5428ec91
FernandoConsolinRosa <128821668+FernandoConsolinRosa11@users.noreply.github.com>
2026-03-23 11:30:08 -0300

    ArquiteturaInicializada

---
+++ src/server.ts
@@ -0,0 +1,21 @@
+import 'dotenv/config';
+import express from 'express';
+import cors from 'cors';
+
+const app = express();
+
+app.use(cors({
+    origin: 'http:localhost:5173' // Somente o FrontEnd terá acesso ao back dessa aplicação
+}));  
+
+app.use(express.json()); // Permite que o express entenda o JSON no body da requisição
+
+const port = Number(process.env.PORT) || 3001; // Usa a porta do .env ou 3001 como fallback
+
+app.get('/', (req, res) => {
+  res.json({ message: "Backend rodando com sucesso!" });
+});
+
+app.listen(port, () => {
+  console.log(`Servidor online em http://localhost:${port}`);
+});
